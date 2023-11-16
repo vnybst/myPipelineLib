42d1bf92ee3e5d6baac10aaed62698f383c6943c
Vinay Bisht <vinay.bisht@amarulasolutions.com>
2023-11-16 13:55:11 +0530

    initial commit

---
+++ src/main/groovy/com/amarula/lib/MyLib.groovy
@@ -0,0 +1,13 @@
+package com.amarula.lib
+
+class MyLib {
+
+    void doSomething(Map options = [:]) {
+        print("Hello ${options.from}")
+        print("Hello ${options.to}")
+    }
+
+    String getSomething() {
+        return "Something"
+    }
+}
